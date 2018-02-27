d95dc71ee0719a171bd24df32d9e2d75111a665b
DacTr <36852774+DacTr@users.noreply.github.com>
2018-02-27 13:52:06 +0100

    Add files via upload

--- SumCube.cpp
+++ SumCube.cpp
@@ -1,33 +1,46 @@
-
-// decompose les nombres allant de 1 à 2000 en somme minimale de cubes. 
-
 #include <iostream>
 #include <map>
 #include <stdlib.h>
 #include <string>
 #include <vector>
 #include <algorithm>
+
+#define M_SIZE 100000
+
 using namespace std;
 
-int dp[2000];
+int dp[M_SIZE];
 int temp;
 map<int, pair<int,int> > parent;
 int index1, index2 = 0;
 int counter, query;
 
+void display(int query, map<int, pair<int, int> > parent)
+{
+    cout << parent[query].second << "^3";
+    counter = parent[query].first;
+    while (counter>0)
+    {
+        cout << " + " << parent[counter].second << "^3";
+        counter = parent[counter].first; 
+    }
+}
+
 
 int main()
 {
     dp[0] = 1;
-    for (int i = 1; i < 2000; i++)
+    parent[1].first = 0;
+    parent[1].second = 1;
+    for (int i = 1; i < M_SIZE; i++)
     {
         dp[i] = i;
     }
 
-    for (int i = 2; i < 2000; i++)
+    for (int i = 2; i < M_SIZE; i++)
     {
         index1, index2 = 0;
-        for (int j = 1; j < 2000; j++)
+        for (int j = 1; j < M_SIZE; j++)
         {
             temp = j*j*j;
             if( temp > i)
@@ -43,13 +56,12 @@ int main()
         parent[i].second = index2;
         //cout << "number : " << i << " --> " << dp[i] << endl;
     }
+
+
     cin >> query;
-    cout << parent[query].second << "^3";
-    counter = parent[query].first;
-    while (counter>0)
-    {
-        cout << " + " << parent[counter].second << "^3";
-        counter = parent[counter].first; 
-    }
+    if (cin.good())
+        display(query, parent);
+    else
+        cout << "Enter number" << endl;
     return 0;
 }
